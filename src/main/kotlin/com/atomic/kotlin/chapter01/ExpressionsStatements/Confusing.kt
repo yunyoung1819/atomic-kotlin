456f735630e8b4a8329c77be49279eb3e6ab1649
yunyoung <yunyoung1819@hanmail.net>
2024-03-21 14:53:53 +0900

    kotlin study

---
+++ src/main/kotlin/com/atomic/kotlin/chapter01/ExpressionsStatements/Confusing.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var i = 1
+    println(i++ + ++i)
+    println(i++ + 10)
+    println(20 * ++i)
+}
---
+++ src/main/kotlin/com/atomic/kotlin/chapter01/ExpressionsStatements/PostfixVsPrefix.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var i = 10
+    println(i++) // 10
+    println(i)   // 11
+
+    var j = 20
+    println(++j) // 21
+    println(j)   // 21
+}
