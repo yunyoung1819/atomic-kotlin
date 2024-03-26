40f1c02ec839abc493ee044309350e11d433e227
yunyoung <yunyoung1819@hanmail.net>
2024-03-27 00:16:15 +0900

    feat: kotlin study object

---
+++ src/main/kotlin/com/atomic/kotlin/chapter02/Object/IntRanges.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val r1 = IntRange(0, 10)
+    val r2 = IntRange(5, 7)
+
+    println(r1)
+    println(r2)
+}
---
+++ src/main/kotlin/com/atomic/kotlin/chapter02/Object/RangeSum.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val r = IntRange(0, 10)
+    println(r.sum())
+}
