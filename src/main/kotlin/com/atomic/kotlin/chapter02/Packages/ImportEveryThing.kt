e822a5010fc6230f98ba54d4179c4cc9930848d1
yunyoung1819 <yunyoung1819@hanmail.net>
2024-04-11 02:38:40 +0900

    feat: kotlin study package

---
+++ src/main/kotlin/com/atomic/kotlin/chapter02/Packages/ImportEveryThing.kt
@@ -0,0 +1,7 @@
+import kotlin.math.*
+
+fun main() {
+    println(E)
+    println(E.roundToInt())
+    println(E.toInt())
+}
---
+++ src/main/kotlin/com/atomic/kotlin/chapter02/Packages/ImportPythagorean.kt
@@ -0,0 +1,7 @@
+import pythagorean.RightTriangle
+
+fun main() {
+    val rt = RightTriangle(3.0, 4.0)
+    println(rt.hypotenuse())
+    println(rt.area())
+}
