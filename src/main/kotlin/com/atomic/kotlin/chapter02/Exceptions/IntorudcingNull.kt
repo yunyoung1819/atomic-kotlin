9c7aac120cbb1a35a7ece92f52063472d322308a
yunyoung <yunyoung1819@hanmail.net>
2024-05-10 02:09:32 +0900

    feat: kotlin study exception

---
+++ src/main/kotlin/com/atomic/kotlin/chapter02/Exceptions/IntorudcingNull.kt
@@ -0,0 +1,7 @@
+package com.atomic.kotlin.chapter02.Exceptions
+
+import com.atomic.kotlin.atomicTest.*
+
+fun main() {
+    "1$".toIntOrNull() eq null
+}
---
+++ src/main/kotlin/com/atomic/kotlin/chapter02/Exceptions/ToIntException.kt
@@ -0,0 +1,9 @@
+package com.atomic.kotlin.chapter02.Exceptions
+
+fun erroneousCode() {
+    // val i = "1$".toInt()
+}
+
+fun main() {
+    erroneousCode()
+}
