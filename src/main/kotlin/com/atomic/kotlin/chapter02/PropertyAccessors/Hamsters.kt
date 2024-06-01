6a681ea85b846c79429fb1b3a58225f652825142
yunyoung <yunyoung1819@hanmail.net>
2024-06-01 18:14:31 +0900

    feat: kotlin study property

--- src/main/kotlin/com/atomic/kotlin/chapter02/PropertyAccessors/Hamsters.kt
+++ src/main/kotlin/com/atomic/kotlin/chapter02/PropertyAccessors/Hamsters.kt
@@ -1,3 +1,41 @@
 package com.atomic.kotlin.chapter02.PropertyAccessors
 
 import com.atomic.kotlin.atomicTest.*
+
+class Hamster(val name: String)
+
+class Cage(private val maxCapacity: Int) {
+    private val hamsters =
+        mutableListOf<Hamster>()
+    val capacity: Int
+        get() = maxCapacity - hamsters.size
+
+    val full: Boolean
+        get() = hamsters.size == maxCapacity
+
+    fun put(hamster: Hamster): Boolean =
+        if (full)
+            false
+        else {
+            hamsters += hamster
+            true
+        }
+
+    fun take(): Hamster = hamsters.removeAt(0)
+}
+
+fun main() {
+    val cage = Cage(2)
+    cage.full eq false
+    cage.capacity eq 2
+
+    cage.put(Hamster("Minji")) eq true
+    cage.put(Hamster("Hani")) eq true
+
+    cage.full eq true
+    cage.capacity eq 0
+
+    cage.put(Hamster("Hearin")) eq false
+    cage.take()
+    cage.capacity eq 1
+}
---
+++ src/main/kotlin/com/atomic/kotlin/chapter02/PropertyAccessors/Hamsters2.kt
@@ -0,0 +1,9 @@
+package com.atomic.kotlin.chapter02.PropertyAccessors
+
+class Cage2(private val maxCapacity: Int) {
+    private val hamsters = mutableListOf<Hamster>()
+
+    fun capacity(): Int = maxCapacity - hamsters.size
+
+    fun isFull(): Boolean = hamsters.size == maxCapacity
+}
