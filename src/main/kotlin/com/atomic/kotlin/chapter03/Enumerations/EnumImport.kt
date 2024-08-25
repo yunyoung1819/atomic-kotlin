605a93d8223082fe50363010f1778370193e232b
yunyoung1819 <yunyoung1819@hanmail.net>
2024-08-25 18:41:35 +0900

    feat: kotlin study enum

---
+++ src/main/kotlin/com/atomic/kotlin/chapter03/Enumerations/EnumImport.kt
@@ -0,0 +1,9 @@
+package com.atomic.kotlin.chapter03.Enumerations
+
+import com.atomic.kotlin.atomicTest.*
+import com.atomic.kotlin.chapter03.Enumerations.Level.*
+
+fun main() {
+    Overflow eq "Overflow"
+    High eq "High"
+}
